73816ab734999aee087e46e1847f7dcb6817f079
ristafn <golemmite23@gmail.com>
2021-02-18 22:42:39 +0700

    add data_types.js and operator.js

---
+++ data_types.js
@@ -0,0 +1,26 @@
+/* Data Types:
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+var myName = "Beau"
+
+myName = 8
+
+let ourName = "freeCodeCamp"
+
+const pi = 3.14
+
+var a //a undefined/unsigned
+var b = 2 //b signed with 2
+
+// example
+var a = 5
+var b = 10
+var c = "I am a"
+
+a = a + 1
+b = b + 5
+c = c + " String!"
+
+// console.log(a,b,c)
+
---
+++ operator.js
@@ -0,0 +1,104 @@
+// adding numbers
+var sum = 10 + 10
+
+// subtracting numbers
+var difference = 45 - 33
+
+// multiplying numbers
+var product = 8 * 10
+
+// dividing numbers
+var quotient = 66 / 33
+
+// incrementing numbers
+var myVar = 87
+
+myVar = myVar + 1
+// myVar++
+
+// decrementing numbers
+myVar = myVar - 1
+// myVar--
+
+// decimal number
+var ourDecimal = 5.7
+var myDecimal = 0.009
+
+// multiply decimals
+var product = 0.2 * 0.0
+
+// divide decimal
+var qoutient = 4.4 / 2.0
+
+// finding the remainder
+var remainder
+remainder = 11 % 3
+// console.log(remainder)
+
+// compound assigment with addition
+var a = 3
+var b = 17
+var c = 12
+
+a += 12
+b += 9
+c += 7
+
+// compound assigment with subtracting
+a -= 12
+b -= 9
+c -= 7
+
+// compound assigment with multiply
+a *= 12
+b *= 9
+c *= 7
+
+// compound assigment with division
+a /= 12
+b /= 9
+c /= 7
+
+// declare string variable
+var firstname = "Beau"
+var lastname = "Carnes"
+
+// escaping literal quotes in string
+var myStr = "I am a \"double quoted\" string inside \"double quotes\""
+// console.log(myStr)
+
+// escaping literal quotes with single quotes
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>'
+// var myStr = `'<a href="http://www.example.com" target="_blank">Link</a>'`
+// console.log(myStr)
+
+// escaping sequences in string
+/*****
+CODE OUTPUT
+\'     single quote
+\"     double quote
+\\     backslash
+\n     newline
+\r     carriage return
+\t     tab
+\b     backspace
+\f     form feed
+*****/
+
+// concating
+var myStr = "This is the start. " + "This is the end."
+// console.log(myStr)
+
+// concating with plus equal op
+var myStr = "This is the first sentence."
+myStr += "This is the second sentence."
+
+// constructing string with variables
+var myName = "Beau"
+var myStr = "My name is " + myName + " and I am well!"
+// console.log(myStr)
+
+// appending variables to string
+var someAdjective = "worthwhile"
+var myStr = "Learning to code is "
+myStr += someAdjective
