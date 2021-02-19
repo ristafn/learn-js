1fe1340816582a24eb228085fec53f7a2cdeb9ae
ristafn <golemmite23@gmail.com>
2021-02-19 19:38:02 +0700

    add function phoneticLookup

--- object.js
+++ object.js
@@ -13,25 +13,45 @@ console.log(myDog)
 var nameMyDog = myDog.name
 console.log(nameMyDog)
 
-// =========================================
-
 // access object properties with bracket notation
 var tailsMyDog = myDog["tails"]
 console.log(tailsMyDog)
-
-// =========================================
 
 // access object properties with variable
 var legsMyDog = "legs"
 console.log(myDog[legsMyDog])
 
-// =========================================
-
 // modify object properties
 myDog.name = "Domge"
 console.log(myDog.name)
 
+// add new properties to object
+myDog["bark"] = "woof!"
+console.log(myDog)
+
+// delete properties in object
+delete myDog.tails
+console.log(myDog)
+
+// using object for lookups
+function phoneticLookup(val) {
+    var result = ""
+
+    // this object will replace the if statement or switch case for some case
+    var lookup = {
+        "alpha": "Adams",
+        "bravo": "Boston",
+        "charlie": "Chicago",
+        "delta": "Denver",
+        "echo": "Easy",
+        "foxtrot": "frank"
+    }
+    result = lookup[val]
+    return result
+}
+
+console.log(phoneticLookup("charlie"))
+
 // =========================================
-// =========================================
-// =========================================
+
 
