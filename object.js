daf7484accf17efa392f27aad28b88252a976192
ristafn <golemmite23@gmail.com>
2021-02-19 19:22:36 +0700

    add object.js

---
+++ object.js
@@ -0,0 +1,37 @@
+var myDog = {
+    "name": "Quincy",
+    "legs": 3,
+    "tails": 2,
+    "friends": []
+}
+
+console.log(myDog)
+
+// =========================================
+
+// access object properties with dot notation
+var nameMyDog = myDog.name
+console.log(nameMyDog)
+
+// =========================================
+
+// access object properties with bracket notation
+var tailsMyDog = myDog["tails"]
+console.log(tailsMyDog)
+
+// =========================================
+
+// access object properties with variable
+var legsMyDog = "legs"
+console.log(myDog[legsMyDog])
+
+// =========================================
+
+// modify object properties
+myDog.name = "Domge"
+console.log(myDog.name)
+
+// =========================================
+// =========================================
+// =========================================
+
