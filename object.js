051cd89d55c20545ba8e09e3eca4f637a0dc5714
ristafn <golemmite23@gmail.com>
2021-02-19 19:56:37 +0700

    add function checkObj and add complex object

--- object.js
+++ object.js
@@ -33,6 +33,8 @@ console.log(myDog)
 delete myDog.tails
 console.log(myDog)
 
+// =========================================
+
 // using object for lookups
 function phoneticLookup(val) {
     var result = ""
@@ -53,5 +55,51 @@ function phoneticLookup(val) {
 console.log(phoneticLookup("charlie"))
 
 // =========================================
+
+// testing the properties in object
+var myObj = {
+    gift: "pony",
+    pet: "kitten",
+    bed: "sleigh"
+}
+
+function checkObj(checkProp){
+    if (myObj.hasOwnProperty(checkProp)) {
+        return myObj[checkProp]
+    } else {
+        return "Not Found"
+    }
+}
+
+console.log(checkObj("gift"))
+// =========================================
+
+// manipulating complex object
+var myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Beau Carnes",
+        "title": "Cereal Man",
+        "release_year": 2003,
+        "formats": [
+            "Youtube video"
+        ],
+        "gold": true
+    }
+]
+
+// =========================================
+
+
 
 
