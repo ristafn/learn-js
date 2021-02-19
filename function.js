2419bc650f2ef0a982e5255b1367ee947a904bdc
ristafn <golemmite23@gmail.com>
2021-02-19 18:58:23 +0700

    add function counting card

--- function.js
+++ function.js
@@ -169,7 +169,7 @@ function caseInSwitch(val) {
             break
         default:
             return null
-            break;
+            break
     }
     return answer
 }
@@ -188,3 +188,34 @@ console.log(abTest(2,2))
 
 // =========================================
 
+// counting card
+var count = 0
+
+function cc(card) {
+    switch(card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++
+            break
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count--
+            break
+    }
+
+    var holdbet = "hold"
+    if (count > 0) {
+        holdbet = "bet"
+    }
+
+    return count + " " + holdbet 
+}
+
+cc(2); cc("K"); cc(10); cc("K"); cc("A")
+console.log(cc(4))
