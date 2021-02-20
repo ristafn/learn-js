c24a4d5196fe5c792d53d14f49dc7750a25a1164
ristafn <golemmite23@gmail.com>
2021-02-20 12:06:16 +0700

    add accessing nested object and nested array object

--- object.js
+++ object.js
@@ -100,6 +100,48 @@ var myMusic = [
 
 // =========================================
 
+// accessing nested object
+var myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passaenger seat": "crumbs"
+        },
+        "outside": {
+            "trunk": "jack"
+        }
+    }
+}
+
+var gloveBoxContents = myStorage.car.inside["glove box"]
+console.log(gloveBoxContents)
+
+// =========================================
+
+// accessing nested array object
+var myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+]
+
+var secondTree = myPlants[1].list[1]
+console.log(secondTree)
+// =========================================
+
 
 
 
