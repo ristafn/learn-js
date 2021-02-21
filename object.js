6c8f922359dd8ab265016416f5a52be4966573f1
ristafn <golemmite23@gmail.com>
2021-02-21 16:11:23 +0700

    add record collection

--- object.js
+++ object.js
@@ -143,6 +143,56 @@ console.log(secondTree)
 
 // =========================================
 
+// record collection
+var collection = {
+    "2548": {
+        "album": "Slippery When Wet",
+        "artist": "Bon Jovi",
+        "tracks": [
+            "Let It Rock",
+            "You Give Love a Bad Name"
+        ]
+    },
+    "2468": {
+        "album": "1999",
+        "artist": "Prince",
+        "tracks": [
+            "1999",
+            "Little Red Corvette"
+        ]
+    },
+    "1245": {
+        "artist": "Robert Palmer",
+        "tracks": [ ]
+    },
+    "5439": {
+        "album": "ABBA Gold"
+    }
+}
+
+/* 
+JSON.parse() --> get string from JSON and change into object js
+JSON.stringify() --> get object form js and change into string JSON
+*/
+var collectionCopy = JSON.parse(JSON.stringify(collection))
+
+function updateRecord(id, prop, value) {
+    if (value == "") {
+        delete collection[id][prop]
+    } else if (prop == "tracks") {
+        collection[id][prop] = collection[id][prop] || []
+        collection[id][prop].push(value)
+    } else {
+        collection[id][prop] = value
+    }
+    return collection
+}
+updateRecord(2468, "tracks", "test")
+console.log(updateRecord(5439, "artist", "ABBA"))
+
+// =========================================
+
+
 
 
 
